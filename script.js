08ee4bed6dbef90cdecf8a2ae15fb55f23f8faab
Jeanmari-CSMT <ellielee747@gmail.com>
2025-05-04 10:11:36 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-elementImg = document.getElementById("idImage");
-
-listImage =["advertisement.png", "gambar kolaborasi stev and jen 2.jpg", "gambar kolaborasi stev and jen.jpg"];
-i=0;
-function gantiGbr(){
-    elementImg.src=listImage[i];
-    i = (i+1)% listImage.length;
-}
-setInterval(gantiGbr,2000)
